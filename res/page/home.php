caa182c40d95a3c349e5925a7fc5d5e68d044c40
Sunu Haeriadi <haeriadi@gmail.com>
2016-10-11 14:01:36 +0700

    Memperbarui interface agar lebih rapi
    [ADD] config-based menu generation
    [ADD] config-based front panel generation. still linked to menu.

--- res/page/home.php
+++ res/page/home.php
@@ -1,42 +1,42 @@
 <div class="uk-grid">
-	<h2 class="uk-align-center">Layanan {{ @site.name }}</h2>
+	<h1 class="uk-align-center">{{ @site.name }}</h1>
 </div>
 
 <ul class="uk-grid uk-grid-width-1-3 uk-container uk-container-center">
-	<li>
-		<div class="uk-panel">
-			<div class="uk-panel-badge uk-badge"><a class="uk-text-contrast" href="/forum">Masuk forum</a></div>
+	<repeat group="{{ @site.services.forum }}" value="{{ @data }}">
+		<li>
+			<div class="uk-panel uk-panel-space uk-panel-hover">
+				<h2 class="uk-panel-title"><span class="{{ @data.2 }}"></span> {{ strtoupper(@data.1) }}</h2>
 
-			<h3 class="uk-panel-title">Forum JVMIT</h3>
+				<p>{{ @data.3 | raw }}</p>
 
-			<p>Forum divisi teknologi dan informasi <strong>Java Multi Mandiri</strong>
-			digunakan untuk diskusi internal mengenai proyek-proyek yang dikembangkan
-			oleh anggota tim.</p>
-		</div>
-	</li>
+				<a class="uk-button uk-button-primary uk-width-1-1" href="/forum/{{ @data.0 }}"><span class="uk-icon-chevron-circle-right"></span> Masuk forum {{ @data.1 }}</a>
+			</div>
+		</li>
+	</repeat>
 
-	<li>
-		<div class="uk-panel">
-			<div class="uk-panel-badge uk-badge"><a class="uk-text-contrast" href="/file/multimedia">Hubungi tim multimedia</a></div>
+	<repeat group="{{ @site.services.request }}" value="{{ @data }}">
+		<li>
+			<div class="uk-panel uk-panel-space uk-panel-hover">
+				<h2 class="uk-panel-title"><span class="{{ @data.2 }}"></span> {{ strtoupper(@data.1) }}</h2>
 
-			<h3 class="uk-panel-title">Request Multimedia</h3>
+				<p>{{ @data.3 | raw }}</p>
 
-			<p>Layanan yang memudahkan semua anggota tim
-			<strong>Java Multi Mandiri</strong> untuk melakukan request
-			kepada tim multimedia.</p>
+				<a class="uk-button uk-button-primary uk-width-1-1" href="/forum/{{ @data.0 }}"><span class="uk-icon-chevron-circle-right"></span> Minta request {{ @data.1 }}</a>
+			</div>
+		</li>
+	</repeat>
 
-		</div>
-	</li>
+	<repeat group="{{ @site.services.file }}" value="{{ @data }}">
+		<li>
+			<div class="uk-panel uk-panel-space uk-panel-hover">
+				<h2 class="uk-panel-title"><span class="{{ @data.2 }}"></span> {{ strtoupper(@data.1) }}</h2>
 
-	<li>
-		<div class="uk-panel">
-			<div class="uk-panel-badge uk-badge"><a class="uk-text-contrast" href="/file/journal">Isi jurnal</a></div>
+				<p>{{ @data.3 | raw }}</p>
 
-			<h3 class="uk-panel-title">Jurnal Harian</h3>
+				<a class="uk-button uk-button-primary uk-width-1-1" href="/forum/{{ @data.0 }}"><span class="uk-icon-chevron-circle-right"></span> Akses file {{ @data.1 }}</a>
+			</div>
+		</li>
+	</repeat>
 
-			<p>Jurnal harian tim internet marketing Divisi IT sebagai alat bantu
-			untuk tracking dan recording proyek yang sedang dikerjakan.</p>
-
-		</div>
-	</li>
 </ul>
--- res/ui/page-navbar.php
+++ res/ui/page-navbar.php
@@ -5,38 +5,37 @@
 
 	<ul class="uk-navbar-nav">
 		<li class="uk-parent" data-uk-dropdown aria-haspopup="true" aria-expanded="false">
-			<a href=""><span class="uk-icon-comments"></span> Forum</a>
+			<a href=""><span class="uk-icon-group"></span> Forum</a>
 
 			<div class="uk-dropdown uk-dropdown-navbar uk-dropdown-bottom">
 				<ul class="uk-nav uk-nav-navbar">
-					<li><a href="/forum/jvmit"><span class="uk-icon-comments"></span> Divisi IT</a></li>
-					<li><a href="/forum/jvmdev"><span class="uk-icon-code"></span> Software Development</a></li>
-				</ul>
-			</div>
-		</li>
-
-
-		<li><a href="/file/multimedia" title="Request multimedia"><span class="uk-icon-cubes"></span> Multimedia</a></li>
-
-		<li class="uk-parent" data-uk-dropdown aria-haspopup="true" aria-expanded="false">
-			<a href=""><span class="uk-icon-sitemap"></span> Tim IT</a>
-
-			<div class="uk-dropdown uk-dropdown-navbar uk-dropdown-bottom">
-				<ul class="uk-nav uk-nav-navbar">
-					<li><a href="/file/journal"><span class="uk-icon-book"></span> Jurnal Harian</a></li>
-					<li><a href="/file/csr"><span class="uk-icon-users"></span> Jurnal CSR</a></li>
-					<li><a href="/file/glite" title="Berkas proyek GLite"><span class="uk-icon-server"></span> Berkas proyek GLite</a></li>
-					<li><a href="/file/tech-support"><span class="uk-icon-code"></span> Website Technical Support</a></li>
+					<repeat group="{{ @site.services.forum }}" value="{{ @data }}">
+						<li><a href="/forum/{{ @data.0 }}"><span class="{{ @data.2 }}"></span> {{ @data.1 }}</a></li>
+					</repeat>
 				</ul>
 			</div>
 		</li>
 
 		<li class="uk-parent" data-uk-dropdown aria-haspopup="true" aria-expanded="false">
-			<a href=""><span class="uk-icon-bar-chart"></span> Grafik</a>
+			<a href=""><span class="uk-icon-plus-square"></span> Request</a>
 
 			<div class="uk-dropdown uk-dropdown-navbar uk-dropdown-bottom">
 				<ul class="uk-nav uk-nav-navbar">
-					<li><a href="/file/alat-ukur-indonesia">Tim Alat Ukur Indonesia</a></li>
+					<repeat group="{{ @site.services.request }}" value="{{ @data }}">
+						<li><a href="/forum/{{ @data.0 }}"><span class="{{ @data.2 }}"></span> {{ @data.1 }}</a></li>
+					</repeat>
+				</ul>
+			</div>
+		</li>
+
+		<li class="uk-parent" data-uk-dropdown aria-haspopup="true" aria-expanded="false">
+			<a href=""><span class="uk-icon-archive"></span> File</a>
+
+			<div class="uk-dropdown uk-dropdown-navbar uk-dropdown-bottom">
+				<ul class="uk-nav uk-nav-navbar">
+					<repeat group="{{ @site.services.file }}" value="{{ @data }}">
+						<li><a href="/forum/{{ @data.0 }}"><span class="{{ @data.2 }}"></span> {{ @data.1 }}</a></li>
+					</repeat>
 				</ul>
 			</div>
 		</li>
